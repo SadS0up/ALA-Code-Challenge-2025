c777414030dd2d1bc7e55dcc99f1c0534e2fb193
David Antonio <davidantoniopetro@gmail.com>
2025-02-11 20:23:08 -0300

    feat: changes on Purchase model

--- API/Models/Purchase.cs
+++ API/Models/Purchase.cs
@@ -2,22 +2,23 @@ namespace API.Models
 {
     public class Purchase
     {
-        public long id { get; set; } = -1;
-        public long userID { get; set; } = -1;
-        public string userName { get; set; } = "";
+        public int id { get; set; } = -1;
+        public int userID { get; set; } = -1;
+        public DateTime orderDate { get; set; } = DateTime.Now;
         public decimal total { get; set; } = -1;
     }
 
     public class PurchaseInsert
     {
-        public long userID { get; set; } = -1;
+        public int userID { get; set; } = -1;
         public decimal total { get; set; } = -1;
+        public DateTime orderDate { get; set; } = DateTime.Now;
     }
 
     public class PurchaseUpdate
     {
-        public long id { get; set; } = -1;
-        public long userID { get; set; } = -1;
+        public int id { get; set; } = -1;
+        public int userID { get; set; } = -1;
         public decimal total { get; set; } = -1;
     }
 }
